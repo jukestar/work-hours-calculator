7c838f98530bbe377adf6f9b987a33e1ec65b0fd
jukestar <steole@gmail.com>
2021-10-22 10:31:03 +0200

    Minor refactor

--- src/app/components/work-hours-calculator/work-hours-calculator.component.ts
+++ src/app/components/work-hours-calculator/work-hours-calculator.component.ts
@@ -1,11 +1,9 @@
 import { Component } from "@angular/core";
 import { AbstractControl, FormControl } from "@angular/forms";
-
-// import moment from 'moment';
 import { defer, Observable, combineLatest } from "rxjs";
 import { distinctUntilChanged, map, startWith } from "rxjs/operators";
 
-export const superChanges = <T>(control: AbstractControl): Observable<T> =>
+export const controlChanges = <T>(control: AbstractControl): Observable<T> =>
     defer(() =>
         control.valueChanges.pipe(
             startWith(control.value),
@@ -22,53 +20,37 @@ export class WorkHoursCalculatorComponent {
     fromControl = new FormControl();
     toControl = new FormControl();
     breakControl = new FormControl(30);
-
     value$: Observable<number>;
-
     timeDurations: number[] = [];
 
     constructor() {
-        const from$ = superChanges<string | null>(this.fromControl);
-        const to$ = superChanges<string | null>(this.toControl);
-        const break$ = superChanges<number | null>(this.breakControl);
+        const from$ = controlChanges<string | null>(this.fromControl);
+        const to$ = controlChanges<string | null>(this.toControl);
+        const break$ = controlChanges<number | null>(this.breakControl);
 
         this.value$ = combineLatest([from$, to$, break$]).pipe(
             map(([from, to, breakValue]) => {
                 const fromDate = new Date(`${"2000-01-01"}T${from}`);
                 const toDate = new Date(`${"2000-01-01"}T${to}`);
-
                 const diffAsSeconds =
-                    Math.abs(fromDate.valueOf() - toDate.valueOf()) / 1000;
-
+                    (toDate.valueOf() - fromDate.valueOf()) / 1000;
                 const breakInSeconds = breakValue * 60;
-
-                // const toMoment = moment(`${'2000-01-01'}T${to}`).subtract(
-                //   breakValue,
-                //   'minutes'
-                // );
-
-                // let diff = toMoment.diff(fromMoment);
-                // let diffAsHoursAndMinutes = moment.utc(diff).format('HH:mm');
-
-                // return diffAsHoursAndMinutes === 'Invalid date'
-                //   ? null
-                //   : diffAsHoursAndMinutes;
-
-                return diffAsSeconds - breakInSeconds;
+                const totalSeconds = diffAsSeconds - breakInSeconds;
+                return totalSeconds > 0 ? totalSeconds : null;
             })
         );
     }
 
-    get summation(): any {
+    get summation(): string | undefined {
         if (
             this.timeDurations === null ||
             this.timeDurations === undefined ||
             this.timeDurations.length === 0
         )
-            return;
+            return undefined;
 
         const totalSeconds = this.timeDurations.reduce((a, b) => a + b);
-        return this.secondsToHHMMSS(totalSeconds);
+        return this.secondsToHHMM(totalSeconds);
     }
 
     addTimeDuration(seconds: number): void {
@@ -78,21 +60,16 @@ export class WorkHoursCalculatorComponent {
         this.toControl.setValue(null);
     }
 
-    hhmmssToSeconds(str: string) {
-        var arr = str.split(":").map(Number);
-        return arr[0] * 3600 + arr[1] * 60;
-    }
-
-    secondsToHHMMSS(seconds: number) {
-        var a = (seconds / 3600).toString();
-        var b = ((seconds / 60) % 60).toString();
-        var hours = parseInt(a, 10);
-        var minutes = parseInt(b, 10);
+    secondsToHHMM(seconds: number) {
+        var secondsAsHours = (seconds / 3600).toString();
+        var secondsAsMinutes = ((seconds / 60) % 60).toString();
+        var hours = parseInt(secondsAsHours, 10);
+        var minutes = parseInt(secondsAsMinutes, 10);
 
         return [hours, minutes]
-            .map(function (i) {
-                return i.toString().length === 2 ? i : "0" + i;
-            })
+            .map((value) =>
+                value.toString().length === 2 ? value : "0" + value
+            )
             .join(":");
     }
 
