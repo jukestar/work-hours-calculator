1e785930bd931f9d3556a060ec24030cd88c7a8b
jukestar <steole@gmail.com>
2021-10-22 19:46:00 +0200

    Use formGroup

--- src/app/components/work-hours-calculator/work-hours-calculator.component.ts
+++ src/app/components/work-hours-calculator/work-hours-calculator.component.ts
@@ -1,7 +1,18 @@
 import { Component } from "@angular/core";
-import { AbstractControl, FormControl } from "@angular/forms";
+import {
+    AbstractControl,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+} from "@angular/forms";
 import { defer, Observable, combineLatest } from "rxjs";
-import { distinctUntilChanged, map, startWith } from "rxjs/operators";
+import {
+    distinctUntilChanged,
+    map,
+    startWith,
+    take,
+    takeUntil,
+} from "rxjs/operators";
 
 export const controlChanges = <T>(control: AbstractControl): Observable<T> =>
     defer(() =>
@@ -11,30 +22,49 @@ export const controlChanges = <T>(control: AbstractControl): Observable<T> =>
         )
     );
 
+interface WorkTime {
+    startTime: string;
+    endTime: string;
+    lunchBreak: number;
+}
+
+interface Dictionary<T> {
+    [key: string]: T;
+}
+
 @Component({
     selector: "app-work-hours-calculator",
     templateUrl: "work-hours-calculator.component.html",
     styleUrls: ["work-hours-calculator.component.scss"],
 })
 export class WorkHoursCalculatorComponent {
+    form: FormGroup;
     fromControl = new FormControl();
     toControl = new FormControl();
     breakControl = new FormControl(30);
     value$: Observable<number>;
     timeDurations: number[] = [];
 
-    constructor() {
-        const from$ = controlChanges<string | null>(this.fromControl);
-        const to$ = controlChanges<string | null>(this.toControl);
-        const break$ = controlChanges<number | null>(this.breakControl);
+    private _initialWorkTime: WorkTime = {
+        startTime: "00:00",
+        endTime: "00:00",
+        lunchBreak: 30,
+    };
 
-        this.value$ = combineLatest([from$, to$, break$]).pipe(
-            map(([from, to, breakValue]) => {
-                const fromDate = new Date(`${"2000-01-01"}T${from}`);
-                const toDate = new Date(`${"2000-01-01"}T${to}`);
+    constructor(builder: FormBuilder) {
+        this.form = this._buildForm(builder, this._initialWorkTime);
+
+        this.value$ = controlChanges(this.form).pipe(
+            map((formValues: any) => {
+                const fromDate = new Date(
+                    `${"2000-01-01"}T${formValues.startTime}`
+                );
+                const toDate = new Date(
+                    `${"2000-01-01"}T${formValues.endTime}`
+                );
                 const diffAsSeconds =
                     (toDate.valueOf() - fromDate.valueOf()) / 1000;
-                const breakInSeconds = breakValue * 60;
+                const breakInSeconds = formValues.lunchBreak * 60;
                 const totalSeconds = diffAsSeconds - breakInSeconds;
                 return totalSeconds > 0 ? totalSeconds : null;
             })
@@ -78,5 +108,21 @@ export class WorkHoursCalculatorComponent {
         this.fromControl.setValue(null);
         this.toControl.setValue(null);
         this.timeDurations = [];
+    }
+
+    private _toFormData(value: WorkTime): any {
+        return {
+            startTime: value.startTime,
+            endTime: value.endTime,
+            lunchBreak: value.lunchBreak,
+        };
+    }
+
+    private _buildForm(builder: FormBuilder, workTime: WorkTime): FormGroup {
+        return builder.group({
+            startTime: workTime.startTime,
+            endTime: workTime.endTime,
+            lunchBreak: workTime.lunchBreak,
+        });
     }
 }
