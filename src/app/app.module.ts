56309a84ea5cc1fd9e97c6a22e6218a87fd695ea
jukestar <steole@gmail.com>
2021-10-22 20:08:41 +0200

    4 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
 
 import { AppComponent } from "./app.component";
 import { WorkHoursCalculatorComponent } from "./components/work-hours-calculator/work-hours-calculator.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatInputModule,
+    ],
     declarations: [AppComponent, WorkHoursCalculatorComponent],
     bootstrap: [AppComponent],
 })
