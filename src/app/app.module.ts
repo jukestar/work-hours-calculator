8f8c9e7ef7133621221061136fd2fac2d8f276b7
jukestar <steole@gmail.com>
2021-10-22 08:11:43 +0200

    Remove hello component, add work-hours-calculator component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
+import { WorkHoursCalculatorComponent } from "./components/work-hours-calculator/work-hours-calculator.component";
 
 @NgModule({
     imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent, HelloComponent],
+    declarations: [AppComponent, WorkHoursCalculatorComponent],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/components/work-hours-calculator/work-hours-calculator.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-work-hours-calculator",
+    templateUrl: "./work-hours-calculator.component.html",
+    styleUrls: ["./work-hours-calculator.component.scss"],
+})
+export class WorkHoursCalculatorComponent {}
